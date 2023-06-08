6fd3112a6b084650f6f17269372177867cb13b7a
Saralyn Ogden <Saralyn.Ogden@gmail.com>
2023-06-08 14:53:39 -0700

    adding method

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -13,7 +13,28 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
-        return false;
+        if (myString.isEmpty() || myString.length() == 1) {
+            return true;
+        }
+
+        char firstChar = myString.charAt(0);
+        char secondChar = myString.charAt(1);
+        boolean isUpper = Character.toUpperCase(secondChar) == secondChar;
+
+        if (Character.toUpperCase(firstChar) != firstChar && isUpper) {
+            return false;
+        }
+
+        for (int i = 2; i < myString.length(); i++) {
+            char currentChar = myString.charAt(i);
+            boolean charIsUpperCase = Character.toUpperCase(currentChar) == currentChar;
+            if (isUpper && !charIsUpperCase ||
+                    !isUpper && charIsUpperCase) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
